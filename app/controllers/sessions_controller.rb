e46cfcf3dfa7fcc6911915dd692eb2c54026f931
Jaime Spacco <jspacco@knox.edu>
2017-06-16 16:20:57 -0500

    added checkbox labels for correct answers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   def show
-    p "OK, totally calling sessions#show!"
     @session = Session.find_by(id: params[:id])
     @course = Course.find_by(id: @session.course_id)
     @questions = Question.where(session_id: @session.id)
@@ -11,7 +10,10 @@ class SessionsController < ApplicationController
     @session = Session.find_by(id: params[:id])
     @course = Course.find_by(id: @session.course_id)
 
-    # Update all of the questions
+    # Update every question with at least one answer checked
+    # FIXME This will not work for clicker questions that have no correct answers,
+    #   because unchecking all checkboxed will not show up in the post/patch params
+    #   at alll.
     params[:correct_answers].each do |question_id, answers|
       question = Question.find_by(id: question_id)
       # First, assign all correct answers to 0
@@ -20,15 +22,15 @@ class SessionsController < ApplicationController
       # Now, re-assign any answers listed as correct answers to 1
       answers.each do |ans|
         case ans
-        when 'a'
+        when 'a', 'A'
           question.correct_a = 1
-        when 'b'
+        when 'b', 'B'
           question.correct_b = 1
-        when 'c'
+        when 'c', 'C'
           question.correct_c = 1
-        when 'd'
+        when 'd', 'D'
           question.correct_d = 1
-        when 'e'
+        when 'e', 'E'
           question.correct_e = 1
         end
       end
@@ -36,10 +38,12 @@ class SessionsController < ApplicationController
     end
 
     # Finally, look up the questions we just updated to make them available to
-    #   to the view
+    #   to the view. We are going to re-direct to the show view, but NOT the
+    #   show controller.
     @questions = Question.where(session_id: @session.id)
 
-    # Basically, this is a re-direct back to sessions#show
+    # Basically, this is a re-direct ONLY for rendering!
+    #   It does NOT call call the show method in this file.
     render :show
   end
 end
