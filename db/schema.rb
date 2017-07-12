8c33937addc3f6d017a19e9d51ebecec1184ccbf
Jaime Spacco <jspacco@knox.edu>
2017-07-12 18:36:14 -0500

    change required to support postgres
    Needed to enable a new extension to support postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170706214115) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.string "term"
