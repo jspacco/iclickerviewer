92cafeda98b2c20fa041c200d14a59c6f7df2b1d
Jaime Spacco <jspacco@knox.edu>
2017-07-21 19:35:45 -0500

    need to load jquery before bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require bootstrap
 //= require turbolinks
 //= require_tree .
 //= require jquery3
 //= require jquery_ujs
+//= require bootstrap
