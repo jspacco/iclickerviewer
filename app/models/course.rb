98a403df63af9eae6587111272edf14092fc61be
Jaime Spacco <jspacco@knox.edu>
2017-08-07 14:20:29 -0500

    added has many through for questions and courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
-  has_many :class_period
+  has_many :class_periods
+  has_many :questions, through: :class_periods
+  # also has many votes through class_period and question
 end
