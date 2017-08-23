ee0687242552aae6072a90e97d74ca370a100a24
Jaime Spacco <jspacco@knox.edu>
2017-08-22 19:24:28 -0500

    split out the TODO list from README.md

--- cmdline/run_parser.rb
+++ cmdline/run_parser.rb
@@ -6,9 +6,9 @@ if __FILE__ == $0
   root = '/Users/jspacco/projects/clickers/data/'
 
   # These either had a different vote count format, or clickers to be ignored
-  parse_course(root, 'KnoxCS142S17', 'CS142', 'Knox', 'spring', 2017, 'Bunde', false)
-  # parse_course(root, 'UCSD.CSE141S17-1', 'CSE141', 'UCSD', 'spring', 2017, 'Porter', false)
-  # parse_course(root, 'UCSD.CSE141S17-2', 'CSE141', 'UCSD', 'spring', 2017, 'Porter', false)
+  # parse_course(root, 'KnoxCS142S17', 'CS142', 'Knox', 'spring', 2017, 'Bunde', false)
+  parse_course(root, 'UCSD.CSE141S17-1', 'CSE141', 'UCSD', 'spring', 2017, 'Porter', false)
+  parse_course(root, 'UCSD.CSE141S17-2', 'CSE141', 'UCSD', 'spring', 2017, 'Porter', false)
 
 
   # parse_course(root, 'UT.CSC108F16-L0101', 'CS108', 'Toronto', 'fall', 2016, 'Tong')
