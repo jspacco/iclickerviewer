2f31445c172565066a8f5962714e34e333c1336f
Errol Kaylor <ebkaylor@knox.edu>
2018-07-13 14:36:35 -0500

    Changed users table in db, Added Google Auth and removed login page
    Now logging in is entirely through google as opposed to making an account on iclickerviewer. Next up is add a verified component and clean the html.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
   root 'courses#index'
   resources :courses, :only => [:index, :show]
   resources :class_periods, :only => [:show, :update,:update_course_hash]
---
+++ db/migrate/20180713193227_add_id_to_user.rb
@@ -0,0 +1,5 @@
+class AddIdToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :id, :primary_key
+  end
+end
