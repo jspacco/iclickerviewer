cb061e053735ff1b8ea339507473adaf2ffb5150
Errol Kaylor <ebkaylor@knox.edu>
2018-07-17 12:40:56 -0500

    Updated Sign-In and Out function
    Now uses devise routes and methods properly.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:session][:username].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to courses_url, action: :index
-    else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render :new
-    end
+  #  user = User.find_by(username: params[:session][:username].downcase)
+  #  if user && user.authenticate(params[:session][:password])
+  #    log_in user
+  #    redirect_to courses_url, action: :index
+  #  else
+  #    flash.now[:danger] = 'Invalid email/password combination'
+  #    render :new
+  #  end
   end
 
   def destroy
-    log_out
+    sign_out
     flash.now[:notice] = 'You logged out'
     redirect_to root_url
   end
+  #  flash.now[:notice] = 'You logged out'
+  #  redirect_to root_url
+  #end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,11 +7,15 @@ module SessionsHelper
   #def log_out
   #  session.delete(:user_id)
   #  @current_user = nil
+  #
   #end
 
   #def current_user
+  #
   #  @current_user ||= User.find_by(id: session[:user_id])
-  #end
+  #  puts "Current user is #{@current_user} or #{User.find_by(id: session[:user_id])}"
+  #nd
+
 
 
   #def logged_in?
