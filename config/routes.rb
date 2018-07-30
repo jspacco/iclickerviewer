613a73047be4dc3e56077f99cc214d5bde127eb9
Jaime Spacco <jspacco@knox.edu>
2018-07-30 17:24:54 -0500

    cleaned up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  #get 'admin/change_verification'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
   root 'courses#index'
   resources :courses, :only => [:index, :show]
@@ -7,13 +6,6 @@ Rails.application.routes.draw do
   resources :questions, :only => [:index, :show, :update]
   resources :data, :only => [:index]
   resources :admin, :only => [:index]
-  #get    '/change_verification' => 'admin'
   patch '/admin', to: 'admin#update'
-
   get    '/update_course_hash/:id', to: 'class_periods#update_course_hash'
-  #get   '/update_course_hash', to: 'class_periods#update_course_hash'
-
-#WHAT LOGAN SAYS:
-#add the post url, then to table # method name
-#that shoudl help with the problem
 end
