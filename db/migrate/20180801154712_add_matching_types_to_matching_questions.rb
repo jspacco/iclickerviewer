c2abf5b24bda36c91e0c48a712a3482f10ef1710
Errol Kaylor <ebkaylor@knox.edu>
2018-08-01 11:11:41 -0500

    Added types of matches to the matching_questions table

---
+++ db/migrate/20180801154712_add_matching_types_to_matching_questions.rb
@@ -0,0 +1,12 @@
+class AddMatchingTypesToMatchingQuestions < ActiveRecord::Migration[5.2]
+  def change
+    add_column :matching_questions, :changed_question_phrasing, :boolean, default: false
+    add_column :matching_questions, :changed_question_values, :boolean, default: false
+    add_column :matching_questions, :changed_info_phrasing, :boolean, default: false
+    add_column :matching_questions, :changed_info_layout, :boolean, default: false
+    add_column :matching_questions, :changed_answers_phrasing, :boolean, default: false
+    add_column :matching_questions, :changed_answers_values, :boolean, default: false
+    add_column :matching_questions, :changed_answers_order, :boolean, default: false
+    add_column :matching_questions, :changed_answers_type, :boolean, default: false
+  end
+end
