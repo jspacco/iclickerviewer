2de42c71d657b0d3b70c342a2e06ae1a4fbf8f03
Jaime Spacco <jspacco@knox.edu>
2018-08-13 20:40:18 -0500

    added draft of a match controller for future AJAX support

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :data, :only => [:index]
   resources :admin, :only => [:index]
   resources :clusters, :only => [:show]
+  resources :match, :only => [:index, :show]
   patch '/admin', to: 'admin#update'
-  get    '/update_course_hash/:id', to: 'class_periods#update_course_hash'
+  get   '/update_course_hash/:id', to: 'class_periods#update_course_hash'
 end
