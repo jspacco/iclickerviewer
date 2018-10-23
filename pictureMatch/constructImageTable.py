1fa15492e314e33af3d0c0e85e129cfa53d8162b
Jaime Spacco <jspacco@knox.edu>
2018-10-23 10:44:49 -0500

    able to process images and also cache the hash and ocr text

--- pictureMatch/constructImageTable.py
+++ pictureMatch/constructImageTable.py
@@ -22,14 +22,16 @@ def containsC( imageName ):
 
   return cIndex != -1
 
+
+
 # will construct the imageTable with empty matchNamesRemoves
 # will return the values of the populated imageTable
 # OUTLAY OF THE METHOD
 # will go through all the pictures in a given directory
 # will calculate hash value for each picture and get text of each of pics
-# 
+#
 # will create a dictionary with {picName, imageBody}
-# 
+#
 # once the table is called it will determine whether to use the method
 # with both imageHashing and ocr or use pureOCR
 #
@@ -43,28 +45,36 @@ def constructImageTable( directoryPathName, preProcess, templateChange ):
   imageObj = None # tuple of ( imageName/path, imageBody)
   imageBody = None # will be the body of what should be in an image
 
-  imageTable = {} #image table will hold image objects in the format of 
+  imageTable = {} #image table will hold image objects in the format of
                   # imageName: imageObject
   # build the imagePq
   # also build a dictionary with the name of the image as the key
   for imagePath in glob.glob( directoryPathName +"/*.jpg"):
 
     if containsC( imagePath ):
-      continue
+        # skip files containing a C in them (these are the bar charts)
+        continue
     image = cv2.imread( imagePath )
-    
+
     #remove MC box
     image = tempRemoveMC.removeMCBox(image)
 
     # imageName is in the format ./../something/L123123123_Q12.jpg
     dotPosition = imagePath.rfind(".")
     slashPosition = imagePath.rfind("/")
-    filename ="{}.jpg".format(imagePath[slashPosition+1:dotPosition] +"_1") 
+    filename ="{}.jpg".format(imagePath[slashPosition+1:dotPosition] +"_1")
+    print("filename: {}".format(filename))
+    fullpath = imagePath
+    # TODO split imagePath up more:
+    # path/to/UIC.CS108F17/L1709011030_Q1.jpg
+    # should be:
+    # UIC.CS108F17/L1709011030_Q1.jpg
+    print("imagePath: {}".format(imagePath))
     cv2.imwrite(filename, image)
-    
+
     # calculate the hashvalue of the pic using phash
     hashVal = imagehash.phash( Image.open(filename) )
-    
+
     # get text from image
     text = imageToText.getText( preProcess, image, filename)
     imageBody = { 'matchNamesRemove': set(), \
@@ -72,14 +82,16 @@ def constructImageTable( directoryPathName, preProcess, templateChange ):
                   'strictMatch': set(), \
                   'hash_value': hashVal, \
                   'ocr_text': text, \
-                  'imageDominant': ( len(text) <= 75 ) }
+                  'imageDominant': ( len(text) <= 75 ),
+                  'filename': imagePath
+                }
     imageObj = ( imagePath, imageBody )
 
     # save the element to the table, text and hash value
     imageTable[imagePath] = imageBody
 
   # end of above for loop
-  
+
   if templateChange == 'y':
     imageTable = pureOcr.generatePairedTableOnlyOcr( imageTable )
   else:
