46330d4958f4af6a57567c7a06754968ef37e439
Jaime Spacco <jspacco@knox.edu>
2018-11-29 15:39:55 -0600

    updated driver2.py

--- pictureMatch/driver2.py
+++ pictureMatch/driver2.py
@@ -1,16 +1,10 @@
 from compareCourseImages import compare_courses
 
-def main():
-    outdir = 'output'
-
-    cs141s = ['KnoxCS141W15', 'KnoxCS141F15-1', 'KnoxCS141F15-2', 'KnoxCS141W16',
-        'KnoxCS141F16-1', 'KnoxCS141F16-2', 'KnoxCS141W17-2']
-    # cs141s = ['test1', 'test2']
-
+def process_classes(classes, outdir):
     # image_comparator = ImageComparator()
     done = set()
-    for dir1 in cs141s:
-        for dir2 in cs141s:
+    for dir1 in classes:
+        for dir2 in classes:
             if dir1 == dir2:
                 # don't compare directories to themselves
                 continue
@@ -29,6 +23,20 @@ def main():
             done.add('{}-{}'.format(dir1, dir2))
             done.add('{}-{}'.format(dir2, dir1))
             print('Finished with {} and {}'.format(dir1, dir2))
+
+def main():
+    outdir = 'output'
+
+    tests = ['test1', 'test2']
+
+    cs141s = ['KnoxCS141W15', 'KnoxCS141F15-1', 'KnoxCS141F15-2', 'KnoxCS141W16',
+        'KnoxCS141F16-1', 'KnoxCS141F16-2', 'KnoxCS141W17-2']
+
+    cse141s = ['UCSD.CSE141F15', 'UCSD.CSE141F4-A', 'UCSD.CSE141S17-1'
+        'UCSD.CSE141F16', 'UCSD.CSE141F4-B', 'UCSD.CSE141S17-2',
+        'KnoxCS201S15', 'KnoxCS201S16']
+
+    process_classes(cse141s, outdir)
 
 
 if __name__ == '__main__':
