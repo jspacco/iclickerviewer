e4ead6936d4d074c4e518bc1807e566e5f6b5823
Jaime Spacco <jspacco@knox.edu>
2018-12-17 10:08:39 -0600

    preparing to merge with master

--- pictureMatch/pureOcr.py
+++ pictureMatch/pureOcr.py
@@ -3,13 +3,12 @@ import commonMethods
 # populates imageTable with strict/potential matches
 # using only the OCR engine
 def generatePairedTableOnlyOcr( imageTable ):
-
   foundOneMatch = False
   singular = []
   # used imagehashing to figure out pairs
   for elemOneKey, elemOneValue in imageTable.items():
     one = elemOneValue["ocr_text"]
-    for elemTwoKey, elemTwoValue in imageTable.items():
+    for elemTwoKey, elemTwoValue in imageTable2.items():
       two = elemTwoValue["ocr_text"]
       if elemOneKey != elemTwoKey:
         ocrDiff = commonMethods.percentageEditDistance(one, two)
@@ -28,3 +27,27 @@ def generatePairedTableOnlyOcr( imageTable ):
   # the singulars are taken care of
   imageTable = commonMethods.addSingulars( imageTable, singular )
   return imageTable
+
+def generatePairedTableOnlyOcr2( imageTable, imageTable2 ):
+  # What format do I want results?
+  # Image1  Image2  confidence (0/1/2)
+  # UIC.CS108F17/L1709171030_Q5.jpg UIC.CS108F18/L1709101035_Q4.jpg
+
+  result = ''
+
+  # used imagehashing to figure out pairs
+  for elemOneKey, elemOneValue in imageTable.items():
+    one = elemOneValue["ocr_text"]
+    for elemTwoKey, elemTwoValue in imageTable2.items():
+      two = elemTwoValue["ocr_text"]
+      if elemOneKey != elemTwoKey:
+        ocrDiff = commonMethods.percentageEditDistance(one, two)
+        if ocrDiff <= 0.3:
+            # 1 means probable match
+            result += "{}\t{}\t1\n".format(elemOneKey, elemTwoKey)
+        elif ocrDiff < 0.26:
+            # 0 means (almost) positive match
+            result += "{}\t{}\t0\n".format(elemOneKey, elemTwoKey)
+
+  # the singulars are taken care of
+  return result
